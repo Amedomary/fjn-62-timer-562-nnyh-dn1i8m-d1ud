5f5b716748369a97aa5eb2b28d7ccf64592b0f9e
Maxim G <amed@mail.ru>
2018-07-31 23:59:34 +0400

    add share js

--- src/js/internal.js
+++ src/js/internal.js
@@ -1,3 +1,3 @@
 @@include('partials/landing_vue.js')
 
-console.log('Если вы нашли баг, отправьте его описание мне на почту iAmed73@yandex.ru');
+console.log('%c%s', 'display: block; color: red; font: 1.6rem/1.3 Tahoma;', 'Если вы нашли баг, отправьте его описание мне на почту iAmed73@yandex.ru');
--- src/js/partials/landing_vue.js
+++ src/js/partials/landing_vue.js
@@ -26,9 +26,9 @@ var appLanding = new Vue({
 
         // Consts
         CONTENTFROMSERVER: {
-            preHeading: 'Жди вместе с друзьями',
-            heading: 'Запусти свой таймер',
-            description: 'На этом сайте ты можешь создать таймер отсчёта до твоего события и поделиться с друзьями :)'
+            preHeading: 'в ожидании',
+            heading: 'День, когда ничего не поменяется',
+            description: 'но это не точно'
         },
 
         stateWasModified: false, // было ло ли изменено состояние
@@ -53,7 +53,7 @@ var appLanding = new Vue({
         newPreHeadingMessage: '', // ячейка для нового пред Заголовка
 
         // Таймер =================
-        finishDate: new Date(2018, 7, 25, 22, 56, 00),  // (year, month, date, hours, minutes, seconds, ms)
+        finishDate: new Date(2018, 7, 14, 4, 00, 00),  // (year, month, date, hours, minutes, seconds, ms)
         monthName: '',
 
         interval: "",
@@ -138,7 +138,7 @@ var appLanding = new Vue({
                 let promptImputY = prompt('Введите Год вашего события', '2018');
                 promptImputY = Number(promptImputY);
                 let promptImputM = prompt('Месяц', '06');
-                promptImputM = Number(promptImputM);
+                promptImputM = Number(promptImputM + 1);
                 let promptImputD = prompt('День', '31');
                 promptImputD = Number(promptImputD);
                 let promptImputH = prompt('Час', '00');
@@ -283,10 +283,82 @@ var appLanding = new Vue({
         colorPick: function () {
             this.styleApp = { '--theme-color': this.color_i };
             this.color_i = this.color_i + 15;
-        }
+        },
         // ! Выбор цвета ==============
 
+        // share
+        shareCreateLink: function () {
+            window.addEventListener('load', function () {
+                var e = document.getElementsByClassName('b-landing__share');
 
+                for (var k = 0; k < e.length; k++) {
+                    if (e[k].className.indexOf('') != -1) {
+                        if (e[k].getAttribute('data-url') != -1) var u = e[k].getAttribute('data-url');
+                        if (e[k].getAttribute('data-title') != -1) var t = e[k].getAttribute('data-title');
+                        if (e[k].getAttribute('data-image') != -1) var i = e[k].getAttribute('data-image');
+                        if (e[k].getAttribute('data-description') != -1) var d = e[k].getAttribute('data-description');
+                        if (e[k].getAttribute('data-path') != -1) var f = e[k].getAttribute('data-path');
+                        if (e[k].getAttribute('data-icons-file') != -1) var fn = e[k].getAttribute('data-icons-file');
+                        if (!f) {
+                            function path(name) {
+                                var sc = document.getElementsByTagName('script'),
+                                    sr = new RegExp('^(.*/|)(' + name + ')([#?]|$)');
+                                for (var p = 0, scL = sc.length; p < scL; p++) {
+                                    var m = String(sc[p].src)
+                                        .match(sr);
+                                    if (m) {
+                                        if (m[1].match(/^((https?|file)\:\/{2,}|\w:[\/\\])/)) return m[1];
+                                        if (m[1].indexOf("/") == 0) return m[1];
+                                        b = document.getElementsByTagName('base');
+                                        if (b[0] && b[0].href) return b[0].href + m[1];
+                                        else return document.location.pathname.match(/(.*[\/\\])/)[0] + m[1];
+                                    }
+                                }
+                                return null;
+                            }
+                            f = path('share42.js');
+                        }
+                        if (!u) u = location.href;
+                        if (!t) t = document.title;
+                        if (!fn) fn = 'icons.png';
+
+                        function desc() {
+                            var meta = document.getElementsByTagName('meta');
+                            for (var m = 0; m < meta.length; m++) {
+                                if (meta[m].name.toLowerCase() == 'description') {
+                                    return meta[m].content;
+                                }
+                            }
+                            return '';
+                        }
+                        if (!d) d = desc();
+                        u = encodeURIComponent(u);
+                        t = encodeURIComponent(t);
+                        t = t.replace(/\'/g, '%27');
+                        i = encodeURIComponent(i);
+                        d = encodeURIComponent(d);
+                        d = d.replace(/\'/g, '%27');
+
+                        var fbQuery = 'u=' + u;
+                        if (i != 'null' && i != '') fbQuery = 's=100&p[url]=' + u + '&p[title]=' + t + '&p[summary]=' + d + '&p[images][0]=' + i;
+
+                        var vkImage = '';
+                        if (i != 'null' && i != '') vkImage = '&image=' + i;
+
+                        var s = new Array('"#" data-count="vk" onclick="window.open(\'//vk.com/share.php?url=' + u + '&title=' + t + vkImage + '&description=' + d + '\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false" title="Поделиться В Контакте"', '"#" data-count="fb" onclick="window.open(\'//www.facebook.com/sharer/sharer.php?u=' + u + '\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false" title="Поделиться в Facebook"');
+
+                        var l = '';
+
+                        for (j = 0; j < s.length; j++) {
+                            var qq = ['b-icon b-icon--share b-icon--vk icon-vk', 'b-icon b-icon--share icon-fb']
+                        l += '<a class="' + qq[j] + '" rel="nofollow" style="display:inline-block;" href=' + s[j] + ' target="_blank"></a>';
+                        }
+
+                        e[k].innerHTML = l;
+                    }
+                };
+            }, false);
+        }
     },
 
     // Вызывается сразу после того как экземпляр был смонтирован
@@ -298,6 +370,8 @@ var appLanding = new Vue({
         this.interval = setInterval(() => {
             this.clockFunc();
         }, 1000);
+        // меняем шейры
+        this.shareCreateLink();
     },
 
 
